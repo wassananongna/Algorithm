f6f338796e7f279b56aec4a45d48953fd2af997f
wassana khaolegsomboon <684259049@webmail.npru.ac.th>
2025-09-06 19:31:40 +0700

    nong

--- Score.java
+++ Score.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
-public class  TestOddEven {
+public class  Score {
     public static void main(String args[] ) {
         Scanner scanner = new Scanner(System.in);
        
-        System.out.print("Enter number : ");
-        int number= scanner.nextInt();
-        if (number %2 == 0) {
+        System.out.print("Enter your Midterm score : ");
+        double Midterm= scanner.nextDouble();
+        System.out.print("Enter your FinalFinal score : ");
+        double Final= scanner.nextDouble();
+        
+        double Totalscore = Midterm+Final ;
+        
+        if (Totalscore>=50) {
 
-            System.out.println ("เป็นจำนวนเต็มบวก");
+            System.out.println ("Your Total score is "+Totalscore +". The result is Pass!!");
         } else {   
-            System.out.println("ไม่ใช่จำนวนเต็มบวก");
+            System.out.println("Your Total score is "+Totalscore +". The result is Fall!!");
             
         }
 
